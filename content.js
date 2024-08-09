e0158334c30600b3382e16399b0724f6d330e2e6
Ayman <109907618+ajawhark@users.noreply.github.com>
2024-08-09 18:39:54 +0100

    added icons and changed size of box

--- content.js
+++ content.js
@@ -12,17 +12,18 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
       } else {
         iframe = document.createElement('iframe');
         iframe.src = chrome.runtime.getURL('iframe.html');
-        iframe.style.cssText = 'position:fixed;top:50%;left:50%;width:300px;height:300px;z-index:10000;border:none;transform:translate(-50%, -50%);border-radius:15px;overflow:hidden;';
+        iframe.style.cssText = 'position:fixed;top:50%;left:50%;width:500px;height:300px;z-index:10000;border:none;transform:translate(-50%, -50%);border-radius:15px;overflow:hidden;';
 
         // Add onload event listener to focus on the iframe's textarea
         iframe.onload = function() {
           const editor = iframe.contentWindow.document.getElementById('editor');
           if (editor) {
             editor.focus(); // Ensure the textarea is focused after iframe is loaded
-          }
+          }22
         };
 
         document.body.appendChild(iframe);
+
       }
       sendResponse({status: "Iframe toggled successfully"});
     } catch (error) {
